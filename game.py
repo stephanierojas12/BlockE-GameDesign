c83502e1d40b6a8bdef5d6f115b1e5fb32ea5e15
stephanierojas12 <stephanierojas1221@gmail.com>
2022-02-16 08:51:44 -0600

    update

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+
 import random, os
 os.system('cls')
 
@@ -5,14 +6,15 @@ os.system('cls')
 
 # Let's make menu a function key word def
 def menu():
-    print('######################################')
-    print('#       Guess A Number Menu          #')
-    print('#                                    #')
-    print('#         1. Choices 1-10            #')
-    print('#         2. Choices 1-50            #')
-    print('#         3. Choices 1-100           #')
-    print('#        Select your choice          #')
-    print('######################################')
+
+    print('###########W############################')
+    print('#          Guess A Number Menu         #')
+    print('#                                      #')
+    print('#         1.  Choices 1-10             #')
+    print('#         2.  Choices 1-50             #')
+    print('#         3.  Choices 1-100            #')
+    print('#           Select your choice         #')
+    print('######################################  ')
 #Checking for correct user input
 
 menu() #calling the function menu 
@@ -20,20 +22,26 @@ check=True
 while check:
     try:
         choice=int(input("Choice: "))
-        check=False
+        if choice>0 and choice<4: 
+            check=False
+        else:
+            print("please enter only a number from 1-2")
     except ValueError:
         print("Sorry, wrong choice, please enter 1 to 3 only")
 
 if choice == 1:
     myNumber= random.randint(1,10)
+    Level=10
 elif choice == 2:
     myNumber= random.randint(1,50)
+    Level=50
 elif choice == 3:
     myNumber= random.randint(1,100)
+    Level=100
 print(choice)
 GameOn=True
 while(GameOn):
-    userGuess=int(input("give me a number"))
+    userGuess=int(input("give me a number from 1 to "+str(Level)))
     if myNumber ==userGuess:
         print("You guessed it!")
         GameOn=False
